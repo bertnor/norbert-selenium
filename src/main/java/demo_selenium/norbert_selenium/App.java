71ee4779971afda84ff74a2a43e1fe4aa5a79ed8
Norbert <norbert.bokwa@gmail.com>
2018-12-24 01:09:11 +0100

    first set up

---
+++ src/main/java/demo_selenium/norbert_selenium/App.java
@@ -0,0 +1,13 @@
+package demo_selenium.norbert_selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
